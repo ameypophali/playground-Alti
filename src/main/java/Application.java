cf491ab5d4867330d8f7d273ec02e957ba1f3409
SFLDPGUSER-23 <amey.pophali@gmail.com>
2018-03-02 10:39:50 -0500

    Adding application run class

---
+++ src/main/java/Application.java
@@ -0,0 +1,15 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("ids596.*")
+public class Application {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SpringApplication.run(Application.class, args);
+	}
+}
+
