2e4efea41552b036cb4f8fc1d891a165ff8a4878
diwas subedi3 <diwas.subedi3@gmail.com>
2018-03-02 11:28:24 -0500

    Controller added

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,7 +3,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("playground.*")
+@ComponentScan("playground.*,com.altemetric.controller")
 public class Application {
 	
 	public static void main(String[] args) {
